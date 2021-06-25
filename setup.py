69074ae22f23e572e237c90582785d07d5b2c91a
Martin Kjellberg <martin.kjellberg@mu.st>
2021-06-25 11:30:11 +0200

    Change version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='django-cloudinary-storage',
-    version='0.4.1',
+    version='must-0.4.2',
     author='Konrad Lisiczynski',
     author_email='klisiczynski@gmail.com',
     description=description,
